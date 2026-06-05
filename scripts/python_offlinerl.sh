fdcd9577005ea07968d0a0f61796105830299377
xyjie37 <jerryjie1016@gmail.com>
2026-06-05 23:44:35 +0800

    移除Markdown文档、bash脚本和nohup.out等无需上传的文件

--- scripts/python_offlinerl.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec /home/jxy/miniconda3/bin/conda run -n offlinerl python "$@"
--- scripts/python_waihai.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export LD_LIBRARY_PATH="/home/jxy/miniconda3/envs/waihai/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-exec /home/jxy/miniconda3/envs/waihai/bin/python "$@"
